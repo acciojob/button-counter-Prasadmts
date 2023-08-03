19c222eab0da0469401366523959b67e765a9d81
Prasadmts <prasadmts2507@gmail.com>
2023-08-03 19:43:30 +0530

    submitting code-Thu Aug 03 2023 19:43:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [number, setNumber] = useState(0);
+
+  // function increment(e) {
+  //   setNumber(e.target.value + 1);
+  // }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {number} times</p>
+        <button onClick={() => setNumber(number + 1)}>Click me</button>
     </div>
   )
 }
